3c9f1bfad9a09403c3253390e230671392da6890
reherger <reherger@gmail.com>
2024-07-16 21:27:35 +0200

    add services

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+
+const nuxtApp = useNuxtApp()
+const router = useRouter()
+
 </script>
 
 <template>
   <Navigation/>
+  <NuxtLink to="/api/login" external>
+    Sign in
+  </NuxtLink>
+  <NuxtLink to="/api/register" external>
+    Sign up
+  </NuxtLink>
   <Container type="fluid">
     <NuxtPage/>
   </Container>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  modules: ['@pinia/nuxt', "usebootstrap"],
+  modules: ['@pinia/nuxt', 'usebootstrap', '@nuxtjs/kinde'],
   devtools: { enabled: true },
   vite: {
     css: {
